ac279b6ef6ee447c61d9caf81f64e9099e3b77eb
Abhinav Raut <abhinavrautcs@gmail.com>
2021-02-28 05:06:07 +0530

    allow authorization header

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
 app.use(function (req, res, next) {
@@ -15,7 +14,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, authorization');
 
     // Pass to next layer of middleware
     next();
@@ -26,6 +25,7 @@ const pasteRoute = require('./routes/paste');
 
 app.use('/account', accountRoute);
 app.use('/paste', pasteRoute);
+app.get('/:pasteUrl', pasteRoute);
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     app.listen(process.env.PORT);
