e6deead8eaf4a78118efe6227fe0db9610ff82f6
Abhinav Raut <abhinavrautcs@gmail.com>
2021-02-28 05:04:08 +0530

    add more navbar menu items

--- client/src/components/MenuItems.js
+++ client/src/components/MenuItems.js
@@ -2,21 +2,42 @@ export const MenuItems = [
     {
         title: 'Home',
         url: '/',
-        cName: 'nav-links'
+        cName: 'nav-links',
+        login: false,
+        showAlways: true
     },
     {
-        title: 'My Pastes',
+        title: 'My pastes',
         url: '/my-pastes',
-        cName: 'nav-links'
+        cName: 'nav-links',
+        login: true,
+        showAlways: false
     },
     {
         title: 'Create new paste',
         url: '/new',
-        cName: 'nav-links'
+        cName: 'nav-links',
+        login: true,
+        showAlways: false
     },
     {
-        title: 'About',
-        url: '#',
-        cName: 'nav-links'
+        title: 'Logout',
+        url: '/logout',
+        cName: 'nav-links',
+        login: true,
+        showAlways: false
+    },
+    {
+        title: 'Sign in',
+        url: '/sign-in',
+        cName: 'nav-links',
+        login: false,
+        showAlways: false
+    }, {
+        title: 'Sign up',
+        url: '/sign-up',
+        cName: 'nav-links',
+        login: false,
+        showAlways: false
     }
 ]
