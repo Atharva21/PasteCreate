8505ef944d6d08893974dd81617be15ee334c013
Abhinav Raut <abhinavrautcs@gmail.com>
2021-02-28 05:07:15 +0530

    more routes to save delete and get paste from url

--- server/routes/paste.js
+++ server/routes/paste.js
@@ -3,9 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const pasteController = require('../controllers/paste');
+const auth = require("../utils/auth");
 
 const router = express.Router();
 
-router.post('/my-pastes', pasteController.getMyPastes);
+router.get('/my-pastes', auth.authenticateRequest, pasteController.getMyPastes);
+router.post('/save-paste', auth.authenticateRequest, pasteController.storePaste);
+router.post('/delete-paste', auth.authenticateRequest, pasteController.deletePaste);
+router.get('/:pasteUrl', auth.softAuthenticate, pasteController.getPasteFromUrl);
 
 module.exports = router;
