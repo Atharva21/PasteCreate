dc49ea86b9ee63ad3438b502e5b90cf15417e1c4
Abhinav Raut <abhinavrautcs@gmail.com>
2021-02-28 05:04:52 +0530

    store new paste form data in state

--- client/src/components/NewPaste.jsx
+++ client/src/components/NewPaste.jsx
@@ -1,16 +1,68 @@
 import "../css/NewPaste.css";
 import { Button } from "./Button";
 import React from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import swal from 'sweetalert';
 const NewPaste = () => {
+    const [isPrivate, setIsPrivate] = React.useState(false);
+    const [inputFields, setInputFields] = React.useState({});
+    const handlePrivateCheckBox = (e) => {
+        setInputFields(prev => {
+            const target = e.target;
+            return {
+                ...prev,
+                [target.name]: !isPrivate
+            }
+        });
+        setIsPrivate(!isPrivate);
+    }
+    const submitNewPaste = async (e) => {
+        e.preventDefault();
+        e.target.reset();
+        try {
+            const result = await axios.post(process.env.REACT_APP_SERVER + "paste/save-paste", {
+                title: inputFields.title,
+                data: inputFields.paste,
+                private: inputFields.private
+            },
+                {
+                    headers: { 'Authorization': 'Bearer ' + Cookies.get('token') }
+                });
+            if (result.status === 200) {
+                swal("Your paste is saved", "", "success");
+                setInputFields({});
+            } else {
+                console.log(result.data)
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleChangeInputField = (e) => {
+        setInputFields(prev => {
+            const target = e.target;
+            return {
+                ...prev,
+                [target.name]: target.value
+            }
+        });
+    }
     return (
         <React.Fragment>
-            <div className='new-paste-container'>
-                <h2>Enter text here</h2>
-                <textarea id="text-paste-area" />
+            <form className='new-paste-container' onSubmit={submitNewPaste}>
+                <textarea placeholder="Paste your text here" id="text-paste-area" name="paste" onChange={(e) => handleChangeInputField(e)} />
+                <label htmlFor="title">Title</label>
+                <input type="text" name="title" id="title" onChange={(e) => handleChangeInputField(e)} />
+                <div className='private-paste-checkbox-container'>
+                    <label htmlFor="private">Private paste</label>
+                    <input type="checkbox" name="private" id="private" onChange={handlePrivateCheckBox} defaultChecked={isPrivate} />
+                </div>
                 <div className='submit-btn-paste'>
                     <Button text="Submit" buttonStyle='btn--outline' />
                 </div>
-            </div>
+            </form>
         </React.Fragment>
     )
 };
