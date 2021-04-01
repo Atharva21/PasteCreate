98253fde985829555573507a8146086309744f01
Abhinav Raut <abhinavrautcs@gmail.com>
2021-04-02 04:08:44 +0530

    use cors middleware to enabler cors

--- server/index.js
+++ server/index.js
@@ -1,24 +1,12 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, authorization');
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors())
 
 const accountRoute = require('./routes/account');
 const pasteRoute = require('./routes/paste');
