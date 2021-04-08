93d5ac4e3ced1192e14daa258e9d175eb47cc503
Abhinav Raut <abhinavrautcs@gmail.com>
2021-04-08 10:02:50 +0530

    change pastecrate to pastecreate

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -3,7 +3,7 @@ const HomePage = () => {
     return (
         <div className='homepage-container'>
             <div className='homepage-text'>
-                <h1>Hi {localStorage.getItem('name')} Welcome to PasteCrate!</h1>
+                <h1>Hi {localStorage.getItem('name')} Welcome to PasteCreate!</h1>
             </div>
             <div className='homepage-text'>
                 <h2>A free paste storing service</h2>
