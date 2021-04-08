a12f70fd0f9b12985f3c7d459cd96d3aacf672ac
Abhinav Raut <abhinavrautcs@gmail.com>
2021-04-08 06:49:09 +0530

    check if user is logged in or not on base component mount

--- client/src/components/Base.jsx
+++ client/src/components/Base.jsx
@@ -1,13 +1,29 @@
 import NavBar from "./Navbar";
 import { useState, useEffect } from "react";
+import axios from "axios";
 const Base = () => {
     const [isSignedIn, setIsSignedIn] = useState(false);
-    useEffect(() => {
-        const loggedInUser = localStorage.getItem("user");
-        if (loggedInUser) {
-            setIsSignedIn(true);
+
+    const checkLoginStatus = async () => {
+        try {
+            const result = await axios.post(process.env.REACT_APP_SERVER + "account/logged-in", {},
+                {
+                    headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') }
+                });
+            if (result.status === 200) {
+                setIsSignedIn(true)
+            } else {
+                setIsSignedIn(true)
+            }
+        } catch (err) {
+            setIsSignedIn(false)
         }
-    }, [])
+    };
+
+    useEffect(() => {
+        checkLoginStatus()
+    }, [checkLoginStatus])
+
     return (
         <NavBar isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn} />
     )
