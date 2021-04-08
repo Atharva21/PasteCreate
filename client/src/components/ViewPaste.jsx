11d091cbfc089a5d39e7c07919f944c19628f496
Abhinav Raut <abhinavrautcs@gmail.com>
2021-04-08 07:38:54 +0530

    bug fix in view paste from URL

--- client/src/components/ViewPaste.jsx
+++ client/src/components/ViewPaste.jsx
@@ -1,54 +1,52 @@
 
 import { useEffect, useState, Fragment } from "react";
 import { Button } from "./Button";
-import Cookies from "js-cookie";
 import axios from "axios";
 import "../css/Main.css";
 import "../css/MyPastes.css";
 import swal from "sweetalert";
 const ViewPaste = (props) => {
-    const [curPaste, setCurPaste] = useState([]);
+    const [currentPaste, setCurrentPaste] = useState([]);
+
     useEffect(() => {
-        if (props.location.pathname) {
-            let config = {
-                headers: {
-                    'Authorization': 'Bearer ' + Cookies.get('token')
-                }
+        let config = {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
             }
-            props.location.pathname = props.location.pathname.replace("/", "");
-            axios.get(process.env.REACT_APP_SERVER + props.location.pathname, config)
-                .then((res) => {
-                    if (res.status === 200) {
-                        setCurPaste(res.data)
-                    }
-                }).catch(err => {
-                    console.log(err);
-                });
         }
+        const pathName = window.location.pathname.replace("/", "");
+        axios.get(process.env.REACT_APP_SERVER + pathName, config)
+            .then((res) => {
+                if (res.status === 200) {
+                    setCurrentPaste(res.data)
+                }
+            }).catch(err => {
+                console.log(err);
+            });
         return () => {
-            setCurPaste([])
+            setCurrentPaste([])
         }
-    }, [props.location])
+    }, [])
+
     const handleCopyText = (text) => {
         navigator.clipboard.writeText(text);
         swal("Text copied to clipboard");
     }
+
     return (
         <div>
-            {curPaste.length === 0 ? (
+            {currentPaste.length === 0 ? (
                 <div className='flex-center'>
                     <h1>Paste not found! <i className='fas fa-frown'></i></h1>
                 </div>
             ) : (
                 <Fragment>
-                    {curPaste.map((item) => (
-                        <Fragment>
-                            <div key={item._id} className='paste'>
-                                <h2>{item.title}</h2>
-                                {item.data}
-                                <Button text={<i className="fas fa-copy"></i>} buttonStyle='btn--outline-flex-end' onClick={() => { handleCopyText(item.data) }} />
-                            </div>
-                        </Fragment>
+                    {currentPaste.map((item, index) => (
+                        <div key={index} className='paste'>
+                            <h2>{item.title}</h2>
+                            {item.data}
+                            <Button text={<i className="fas fa-copy"></i>} buttonStyle='btn--outline-flex-end' onClick={() => { handleCopyText(item.data) }} />
+                        </div>
                     ))}
                 </Fragment>
             )}
