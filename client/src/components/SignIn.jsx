dd6d1f774130013e49833f54cfe2249b72baa0a1
Abhinav Raut <abhinavrautcs@gmail.com>
2021-05-15 21:16:50 +0530

    shoiw loader on sign in

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -2,15 +2,20 @@ import "../css/SignInSignUp.css";
 import { Button } from "./Button";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import { useLoader, useUpdateLoader } from "./LoadingContext";
 import swal from "sweetalert";
+import Spinner from "./Spinner";
 const SignIn = ({ setIsSignedIn }) => {
-    let history = useHistory();
+    let history = useHistory()
+    const isLoading = useLoader()
+    const setLoader = useUpdateLoader()
     const handleSignIn = async (e) => {
         e.preventDefault();
         const payload = {
             email: e.target.email.value,
             password: e.target.password.value,
         }
+        setLoader(true)
         try {
             const res = await axios.post(process.env.REACT_APP_SERVER + 'account/sign-in', payload);
             if (res.status === 200) {
@@ -20,22 +25,29 @@ const SignIn = ({ setIsSignedIn }) => {
                 setIsSignedIn(true);
                 history.push("/");
             } else {
+                setLoader(false)
                 swal("Invalid credentials");
             }
         } catch (err) {
+            setLoader(false)
             swal("Invalid credentials");
         }
     }
     return (
-        <div className='form-container'>
-            <form className="sign-up-form" onSubmit={handleSignIn}>
-                <label htmlFor="email">Email</label>
-                <input required type="email" name="email" id="email" className='form-item' />
-                <label htmlFor="password">Password</label>
-                <input required type="password" name="password" id="password" className='form-item' />
-                <Button text="Sign in" buttonStyle='btn--outline' className='form-item' />
-            </form>
-        </div>
+        <>
+            {isLoading &&
+                <Spinner />
+            }
+            <div className='form-container'>
+                <form className="sign-up-form" onSubmit={handleSignIn}>
+                    <label htmlFor="email">Email</label>
+                    <input required type="email" name="email" id="email" className='form-item' />
+                    <label htmlFor="password">Password</label>
+                    <input required type="password" name="password" id="password" className='form-item' />
+                    <Button text="Sign in" buttonStyle='btn--outline' className='form-item' />
+                </form>
+            </div>
+        </>
     )
 };
 
